d23f1d6ff36a8d03c50890a75db66a245cd9ccd8
Kalana Thejitha <istore221@gmail.com>
2017-11-22 14:21:31 +0530

    xx

--- js/index.js
+++ js/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import Hello from './components/Hello'
 
 
-ReactDOM.render(<Hello str="hello world package version update"/>, document.getElementById('app'));
+ReactDOM.render(<Hello str="hello world package version 2 update"/>, document.getElementById('app'));
