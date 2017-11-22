513e52888ee601f5c6feae17a525cac82045ee61
Kalana Thejitha <istore221@gmail.com>
2017-11-22 12:21:17 +0530

    dds

--- js/index.js
+++ js/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import Hello from './components/Hello'
 
 
-ReactDOM.render(<Hello str="Hello World react"/>, document.getElementById('app'));
+ReactDOM.render(<Hello str="Hello World react wtf"/>, document.getElementById('app'));
