5f414e021805dafc971a37b6545beca20940f544
Kalana Thejitha <istore221@gmail.com>
2017-11-22 16:27:18 +0530

    xx

--- js/index.js
+++ js/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './components/Hello'
 
-
-ReactDOM.render(<Hello str="salalala"/>, document.getElementById('app'));
+ReactDOM.render(<Hello str="wtf"/>, document.getElementById('app'));
