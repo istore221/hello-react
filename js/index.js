4fd65fe39ea0c08499a613c27a6bd16a7f659c70
Kalana Thejitha <istore221@gmail.com>
2017-11-23 11:54:40 +0530

    changed

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './components/Hello'
 
-ReactDOM.render(<Hello str="Devops Awesome"/>, document.getElementById('app'));
+ReactDOM.render(<Hello str="Devops Devops Everywhere"/>, document.getElementById('app'));
