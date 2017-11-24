666994186db227a10477dccb91701d2ed6a131c6
Kalana Thejitha <istore221@gmail.com>
2017-11-24 10:30:06 +0530

    asa

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './components/Hello'
 
-ReactDOM.render(<Hello str="salalalalalaaaa"/>, document.getElementById('app'));
+ReactDOM.render(<Hello str="Devops Server is Runing"/>, document.getElementById('app'));
