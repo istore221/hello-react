84d03f890df36affc14f990033a05eb9aa012d79
Kalana Thejitha <istore221@gmail.com>
2017-11-24 10:25:32 +0530

    salala

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './components/Hello'
 
-ReactDOM.render(<Hello str="React with love  <3"/>, document.getElementById('app'));
+ReactDOM.render(<Hello str="salalalalalaaaa"/>, document.getElementById('app'));
